779d3d9feb6d493ba12b8fc2ddc370487afc4b6c
hasibulhasan44 <hasanhasib7367@gmail.com>
2022-10-12 20:05:16 +0600

    added some features

--- hustlers-quiz/src/components/SingleQuestion/SingleQuestion.js
+++ hustlers-quiz/src/components/SingleQuestion/SingleQuestion.js
@@ -5,7 +5,6 @@ import {toast} from 'react-toastify'
 const SingleQuestion = (props) => {
   // console.log(props.question);
   const {options , correctAnswer , question} = props.question;
-  console.log(correctAnswer);
   const removePTag = question.replace('<p>', '');
   const mainQuestion = removePTag.replace('</p>', '')
 
@@ -23,9 +22,9 @@ const SingleQuestion = (props) => {
         <div className=" border-2 border-slate-700 lg:w-1/2 mx-auto mb-8 rounded-lg mt-4">
             <p className="bg-cyan-700 text-white text-center p-4 text-lg rounded-lg">{mainQuestion}</p>
             <div className="grid grid-cols-2 p-4 gap-4">
-                {options.map((option) => 
+                {options.map((option , idx) => 
                   <Options 
-                  key={option.id}
+                  key={idx}
                   option={option}
                   handlerForCompare = {compareCorrectAnswer}
                   ></Options>
